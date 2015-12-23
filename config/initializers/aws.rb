03ae56deb65dd96ec603dafd408a05b41b0a1989
Dave Marr <davemarr@gmail.com>
2015-12-23 12:49:02 -0800

    update aws region

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,9 +1,7 @@
-#require 'aws-sdk'
-
 #Aws.config[:stub_responses] = true if Rails.env.test?
 #Aws.config[:credentials] = Aws::Credentials.new(
   #Rails.application.secrets.s3_access_key_id,
   #Rails.application.secrets.s3_secret)
-#Aws.config[:region] = Rails.application.secrets.s3_region
+Aws.config[:region] = Rails.application.secrets.s3_region
 #Aws.config[:log_level] = Rails.logger.level
 #Aws.config[:logger] = Rails.logger
