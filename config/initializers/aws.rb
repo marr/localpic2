55a240869353638d4a196f8c4c442dee64facb9e
Dave Marr <davemarr@gmail.com>
2015-12-23 13:18:24 -0800

    env

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -2,6 +2,6 @@
 #Aws.config[:credentials] = Aws::Credentials.new(
   #Rails.application.secrets.s3_access_key_id,
   #Rails.application.secrets.s3_secret)
-Aws.config[:region] = Rails.application.secrets.s3_region
+#Aws.config[:region] = Rails.application.secrets.s3_region
 #Aws.config[:log_level] = Rails.logger.level
 #Aws.config[:logger] = Rails.logger
